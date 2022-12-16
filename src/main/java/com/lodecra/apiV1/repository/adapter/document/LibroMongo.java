36d2b3ecb40e0ebb771cc365687315f8de9ee0b6
Julio César <juliocesareb@hotmail.com>
2022-12-16 17:03:47 +0100

    Quitado índice

--- src/main/java/com/lodecra/apiV1/repository/adapter/document/LibroMongo.java
+++ src/main/java/com/lodecra/apiV1/repository/adapter/document/LibroMongo.java
@@ -1,11 +1,9 @@
 package com.lodecra.apiV1.repository.adapter.document;
 
-import org.springframework.data.mongodb.core.index.CompoundIndex;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 @Document(value = "libroscollection")
-@CompoundIndex(def = "{'autor':1, 'titulo':1, 'editorial':1, 'contacto':1}", name = "general")
 public record LibroMongo (@Field String codigo,
                           @Field String titulo,
                           @Field String autor,
