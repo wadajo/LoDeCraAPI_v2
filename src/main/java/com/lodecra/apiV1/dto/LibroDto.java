422d4dbc60f1658c5d0a8f1b742a7881a2364bfa
Julio César <juliocesareb@hotmail.com>
2022-12-16 17:03:01 +0100

    Estilo con saltos de línea

--- src/main/java/com/lodecra/apiV1/dto/LibroDto.java
+++ src/main/java/com/lodecra/apiV1/dto/LibroDto.java
@@ -2,4 +2,11 @@ package com.lodecra.apiV1.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
-public record LibroDto(String code, String name, String author, Integer price, String publisher, String contact, @JsonInclude(JsonInclude.Include.NON_NULL) Integer stock, @JsonInclude(JsonInclude.Include.NON_NULL) Boolean discarded) {}
+public record LibroDto(String code,
+                       String name,
+                       String author,
+                       Integer price,
+                       String publisher,
+                       String contact,
+                       @JsonInclude(JsonInclude.Include.NON_NULL) Integer stock,
+                       @JsonInclude(JsonInclude.Include.NON_NULL) Boolean discarded) {}
--- src/main/java/com/lodecra/apiV1/repository/adapter/document/LibroMongo.java
+++ src/main/java/com/lodecra/apiV1/repository/adapter/document/LibroMongo.java
@@ -1,7 +1,16 @@
 package com.lodecra.apiV1.repository.adapter.document;
 
+import org.springframework.data.mongodb.core.index.CompoundIndex;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 @Document(value = "libroscollection")
-public record LibroMongo (@Field String codigo, @Field String titulo, @Field String autor, @Field Integer precio, @Field String editorial, @Field String contacto, @Field Integer stock, @Field Boolean descartado) {}
+@CompoundIndex(def = "{'autor':1, 'titulo':1, 'editorial':1, 'contacto':1}", name = "general")
+public record LibroMongo (@Field String codigo,
+                          @Field String titulo,
+                          @Field String autor,
+                          @Field Integer precio,
+                          @Field String editorial,
+                          @Field String contacto,
+                          @Field Integer stock,
+                          @Field Boolean descartado) {}
