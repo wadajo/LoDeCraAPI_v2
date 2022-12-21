d9e63314241fb4f440f1524e34526ba815648401
JESBA8F <jestravis@paradigmadigital.com>
2022-12-21 13:39:01 +0100

    Cambios e implementación búsqueda general y avanzada

--- src/main/java/com/lodecra/apiV1/repository/adapter/mongo/LibroRepositoryImplMongo.java
+++ src/main/java/com/lodecra/apiV1/repository/adapter/mongo/LibroRepositoryImplMongo.java
@@ -5,13 +5,10 @@ import com.lodecra.apiV1.model.Libro;
 import com.lodecra.apiV1.repository.adapter.document.LibroMongo;
 import com.lodecra.apiV1.repository.port.LibroRepository;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import org.springframework.data.mongodb.core.query.*;
 
 @Repository
 @Primary
@@ -19,12 +16,9 @@ public class LibroRepositoryImplMongo implements LibroRepository {
     private final LibroMongoRepository mongoRepository;
     private final LibroMapper mapper;
 
-    private final MongoTemplate mongoTemplate;
-
-    public LibroRepositoryImplMongo(LibroMongoRepository mongoRepository, LibroMapper mapper, MongoTemplate mongoTemplate) {
+    public LibroRepositoryImplMongo(LibroMongoRepository mongoRepository, LibroMapper mapper) {
         this.mongoRepository = mongoRepository;
         this.mapper = mapper;
-        this.mongoTemplate = mongoTemplate;
     }
     @Override
     public List<Libro> obtenerTodosLosLibros(){
@@ -34,27 +28,49 @@ public class LibroRepositoryImplMongo implements LibroRepository {
 
     @Override
     public List<Libro> obtenerLibrosPorBusquedaGeneral(String keyword) {
-        var todosLosLibrosMongo = mongoRepository.findAll();
-        var librosBuscados = todosLosLibrosMongo.stream().filter(libro->
-                (null!=libro.autor()&&libro.autor().contains(keyword))
-            ||(null!=libro.titulo()&&libro.titulo().contains(keyword))
-            ||(null!=libro.editorial()&&libro.editorial().contains(keyword))
-            ||(null!=libro.contacto()&&libro.contacto().contains(keyword))
-        ).toList();
-        return convertirListaLibrosMongoALibros(librosBuscados);
+        var librosEncontrados = filtrarLibrosPorKeywordGeneral(keyword, mongoRepository.findAll());
+        return convertirListaLibrosMongoALibros(librosEncontrados);
     }
 
     @Override
     public List<Libro> obtenerLibrosPorBusquedaAvz(String keyword, String campoABuscar) {
-        Query query = new Query();
-        query.addCriteria(Criteria.where(campoABuscar).is(keyword));
-        var librosBuscados = mongoTemplate.find(query, LibroMongo.class);
-        return convertirListaLibrosMongoALibros(librosBuscados);
+        var librosEncontrados = filtrarLibrosPorKeywordAvz(keyword,campoABuscar,mongoRepository.findAll());
+        return convertirListaLibrosMongoALibros(librosEncontrados);
     }
 
     private List<Libro> convertirListaLibrosMongoALibros(List<LibroMongo> original){
         List<Libro> aDevolver = new ArrayList<>();
         original.forEach(libroMongo -> aDevolver.add(mapper.libroMongoToLibro(libroMongo)));
         return aDevolver;
+    }
+
+    private List<LibroMongo> filtrarLibrosPorKeywordGeneral(String keyword, List<LibroMongo> todosLosLibrosMongo) {
+        String keywordLC=keyword.toLowerCase();
+        return todosLosLibrosMongo.stream().filter(libro ->
+                (null != libro.autor() && libro.autor().toLowerCase().contains(keywordLC))
+                        || (null != libro.titulo() && libro.titulo().toLowerCase().contains(keywordLC))
+                        || (null != libro.editorial() && libro.editorial().toLowerCase().contains(keywordLC))
+                        || (null != libro.contacto() && libro.contacto().toLowerCase().contains(keywordLC))
+        ).toList();
+    }
+
+    private List<LibroMongo> filtrarLibrosPorKeywordAvz(String keyword, String campoABuscar, List<LibroMongo> todosLosLibrosMongo) {
+        String keywordLC=keyword.toLowerCase();
+        return todosLosLibrosMongo.stream().filter(libro -> switch (campoABuscar){
+            case "autor":
+                if(null != libro.autor() && libro.autor().toLowerCase().contains(keywordLC))
+                    yield true;
+            case "titulo":
+                if(null != libro.titulo() && libro.titulo().toLowerCase().contains(keywordLC))
+                    yield true;
+            case "editorial":
+                if(null != libro.editorial() && libro.editorial().toLowerCase().contains(keywordLC))
+                    yield true;
+            case "contacto":
+                if(null != libro.contacto() && libro.contacto().toLowerCase().contains(keywordLC))
+                    yield true;
+            default:
+                yield false;
+        }).toList();
     }
 }
