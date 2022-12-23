4ec6b730b7600c0cd93d22d2105042c3c3398919
jestravis <jestravis@paradigmadigital.com>
2022-12-23 13:47:05 +0100

    Nuevos mapeos

--- src/main/java/com/lodecra/apiV1/mapstruct/mappers/LibroMapper.java
+++ src/main/java/com/lodecra/apiV1/mapstruct/mappers/LibroMapper.java
@@ -21,8 +21,21 @@ public interface LibroMapper {
     @Mapping(target = "discarded", source = "descartado", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL)
     LibroDto libroToLibroDto(Libro libro);
 
+    @Mapping(target = "codigo", source = "code")
+    @Mapping(target = "titulo", source = "name", defaultValue = "N/A")
+    @Mapping(target = "autor", source = "author", defaultValue = "N/A")
+    @Mapping(target = "precio", source = "price", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL)
+    @Mapping(target = "editorial", source = "publisher", defaultValue = "N/A")
+    @Mapping(target = "contacto", source = "contact", defaultValue = "N/A")
+    @Mapping(target = "stock", source = "stock", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL)
+    @Mapping(target = "descartado", source = "discarded", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL)
+    Libro libroDtoToLibro(LibroDto libroDto);
+
     @Mapping(target = "stock", source = "stock", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL)
     @Mapping(target = "descartado", source = "descartado", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL)
     Libro libroMongoToLibro(LibroMongo libroMongo);
+
+    @Mapping(target = "codigo", expression = "java(com.lodecra.apiV1.util.Utilidades.construirCodigo(55,titulo,autor))")
+    LibroMongo libroToLibroMongo(Libro libro);
 
 }
