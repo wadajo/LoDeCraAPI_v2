15d52992edd4b8d774f92084013e660fbc396d34
Julio César <juliocesareb@hotmail.com>
2023-01-08 13:40:04 +0100

    Corregido el repositorio con método más adecuado

--- src/main/java/com/lodecra/apiV1/repository/adapter/mongo/EjemplarMongoRepository.java
+++ src/main/java/com/lodecra/apiV1/repository/adapter/mongo/EjemplarMongoRepository.java
@@ -3,10 +3,13 @@ package com.lodecra.apiV1.repository.adapter.mongo;
 import com.lodecra.apiV1.repository.adapter.document.EjemplarMongo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface EjemplarMongoRepository extends MongoRepository<EjemplarMongo,String> {
 
     Optional<EjemplarMongo> findByCodLibroAndNroEjemplar(String codLibro, Integer nroEjemplar);
+
+    Optional<List<EjemplarMongo>> findAllByCodLibro(String codLibro);
 
 }
--- src/main/java/com/lodecra/apiV1/repository/adapter/mongo/EjemplarRepositoryImplMongo.java
+++ src/main/java/com/lodecra/apiV1/repository/adapter/mongo/EjemplarRepositoryImplMongo.java
@@ -4,7 +4,6 @@ import com.lodecra.apiV1.mapstruct.mappers.EjemplarMapper;
 import com.lodecra.apiV1.model.Ejemplar;
 import com.lodecra.apiV1.repository.port.EjemplarRepository;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -17,17 +16,19 @@ public class EjemplarRepositoryImplMongo implements EjemplarRepository {
     private final EjemplarMongoRepository mongoRepository;
     private final EjemplarMapper mapper;
 
-    public EjemplarRepositoryImplMongo(EjemplarMongoRepository mongoRepository, EjemplarMapper mapper, MongoTemplate mongoTemplate) {
+    public EjemplarRepositoryImplMongo(EjemplarMongoRepository mongoRepository, EjemplarMapper mapper) {
         this.mongoRepository = mongoRepository;
         this.mapper = mapper;
     }
 
     @Override
     public Optional<List<Ejemplar>> obtenerEjemplaresPorCodigoDeLibro(String codLibro) {
-        var todosLosEjemplares=mongoRepository.findAll();
-        var ejemplaresEsteLibro=todosLosEjemplares.stream().filter(ej->ej.codLibro().equalsIgnoreCase(codLibro)).toList();
+        var todosLosEjemplares=mongoRepository.findAllByCodLibro(codLibro);
         List<Ejemplar> listaEjemplar=new ArrayList<>();
-        ejemplaresEsteLibro.forEach(ejemplarMongo -> listaEjemplar.add(mapper.ejemplarMongoToEjemplar(ejemplarMongo)));
-        return Optional.of(listaEjemplar);
+        todosLosEjemplares.ifPresent(ejemplaresMongo -> ejemplaresMongo.forEach(
+                ejemplarMongo -> listaEjemplar.add(mapper.ejemplarMongoToEjemplar(ejemplarMongo))));
+        return listaEjemplar.isEmpty() ?
+                Optional.empty() :
+                Optional.of(listaEjemplar);
     }
 }
