edc38e810efd2768f43730632f35be13ea521515
Julio César <juliocesareb@hotmail.com>
2023-01-26 21:27:30 +0100

    Libros por búsqueda gral en GraphQL

--- src/main/java/com/lodecra/apiV1/controller/LibroGraphqlController.java
+++ src/main/java/com/lodecra/apiV1/controller/LibroGraphqlController.java
@@ -2,13 +2,16 @@ package com.lodecra.apiV1.controller;
 
 import com.lodecra.apiV1.dto.BookDto;
 import com.lodecra.apiV1.mapstruct.mappers.LibroMapper;
+import com.lodecra.apiV1.model.Libro;
 import com.lodecra.apiV1.service.port.LibroService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Flux;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @Slf4j
@@ -25,12 +28,19 @@ public class LibroGraphqlController {
 
     @QueryMapping
     public Flux<BookDto> booksAvailable(){
-        var todosLosLibrosDto=new ArrayList<BookDto>();
-        var librosDisponibles=libroService.getLibrosDisponibles();
-        librosDisponibles.forEach(libro-> todosLosLibrosDto.add(mapper.libroToBookDto(libro)));
-        return Flux.fromIterable(todosLosLibrosDto);
+        return listaDeLibrosAFluxDeDtos(libroService.getLibrosDisponibles());
     }
 
+    @QueryMapping
+    public Flux<BookDto> booksContainingKeyword(@Argument String keyword){
+        return listaDeLibrosAFluxDeDtos(libroService.getLibrosDisponiblesPorBusquedaGral(keyword));
+    }
+
+    private Flux<BookDto> listaDeLibrosAFluxDeDtos(List<Libro> listaDeLibros){
+        var listaDeDtos=new ArrayList<BookDto>();
+        listaDeLibros.forEach(libro-> listaDeDtos.add(mapper.libroToBookDto(libro)));
+        return Flux.fromIterable(listaDeDtos);
+    }
 
 }
 
