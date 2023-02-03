56a707f3719e360365adc4bad118663d3ed05d53
jestravis <juliocesareb@hotmail.com>
2023-02-03 12:23:05 +0100

    Cambio a config mínima con MongoClientSettingsBuilderCustomizer

--- src/main/java/com/lodecra/apiV1/ApiV2Application.java
+++ src/main/java/com/lodecra/apiV1/ApiV2Application.java
@@ -1,7 +1,11 @@
 package com.lodecra.apiV1;
 
+import com.mongodb.ConnectionString;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoClientSettingsBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiV2Application {
@@ -10,4 +14,9 @@ public class ApiV2Application {
 		SpringApplication.run(ApiV2Application.class, args);
 	}
 
+	@Bean
+	public MongoClientSettingsBuilderCustomizer customizer(@Value("${custom.uri}") String uri) {
+		ConnectionString connection = new ConnectionString(uri);
+		return settings -> settings.applyConnectionString(connection);
+	}
 }
